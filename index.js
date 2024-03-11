d4705f75eebd9070a6ab73ff9e28bd33dd18fd91
AlfianKamil9 <rifqialfiansyahk@gmail.com>
2024-03-11 16:29:13 +0700

    update

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
 const express = require('express');
 const routes = require('./routes');
+const process = require('process');
 const cors = require('cors');
-const expressLayouts = require('express-ejs-layouts');
+
 const bodyParser = require('body-parser');
 //const { loading } = require('./ml');
 
-let model;
 (async () => {
   const app = express();
-  const port = 8000;
+  const port = process.env.PORT || 8000;
 
-  app.set('view engine', 'ejs');
-  app.use(expressLayouts);
   app.use(cors({ origin: ['*'] }));
   app.use(bodyParser.json());
   app.use(routes);
 
-  app.set('layout', 'partials/main-layout');
-
   app.listen(port, () => {
+    console.log(process.env.PORT);
     console.log(`Server running on port http://localhost:${port}`);
   });
 })();
-
-module.exports = model;
